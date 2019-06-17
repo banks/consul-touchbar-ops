362a6b38f653ac6fb1d16a45b05583dfe8432709
Paul Banks <banks@banksco.de>
2019-06-17 21:47:55 +0100

    Initial Commit

---
+++ ConsulTouchOps/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ConsulTouchOps
+//
+//  Created by Paul Banks on 17/06/2019.
+//  Copyright © 2019 banks. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
